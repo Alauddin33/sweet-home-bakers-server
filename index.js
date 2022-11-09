c490b085456eacd3c16c63ef7634552a954cf0f7
Alauddin33 <shamem33@gmail.com>
2022-11-09 18:22:30 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -19,12 +19,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6owueqa.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+
+
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('sweet home bakers is running')
