b7c59c51f33ef576123c87fc1787e6245cfc610a
Alauddin33 <shamem33@gmail.com>
2022-11-10 09:23:37 +0600

    review api create

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ async function run() {
     try {
         const serviceCollection = client.db('sweetHome').collection('services');
 
+        const reviewCollection = client.db('sweetHome').collection('reviews');
+
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -38,6 +41,15 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service);
         })
+
+        // reviews api
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+
 
     }
     finally {
